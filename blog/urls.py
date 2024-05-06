86dc97838813bdfbe8a54cb04f4f80bc83f338d7
abernix102 <nahuelo841@gmail.com>
2024-05-06 13:47:28 -0300

    changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Blog_list_view
+from .views import Blog_list_view, Blog_create_view
 app_name = 'blog'
 
 urlpatterns = [
-    path('', Blog_list_view.as_view(), name='home')
+    path('', Blog_list_view.as_view(), name='home'),
+    path('create/', Blog_create_view.as_view(), name='create')
 ]   
--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,14 @@ from .forms import Post_Create_Forms
 
 class Blog_list_view(View):
     def get(self, request, *args, **kwargs):
-        context ={
-            
-        }
+        context ={}
         return render(request, 'Blog_list.html', context)
 
 class Blog_create_view(View):
     def get(self, request, *arg, **kwargs):
+        form=Post_Create_Forms()
         context ={
-            
+            'form': form
         }
         return render(request, 'blog_create.html', context)
     def post(self, request, *arg, **kwargs):
