80f849becb2ea6bb8dc445c4896f1b35e90d2a5e
abernix102 <nahuelo841@gmail.com>
2024-05-09 05:11:58 -0300

    changes

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View 
 # Create your views here.
 from .forms import Post_Create_Forms
+from .models import Post
 
 class Blog_list_view(View):
     def get(self, request, *args, **kwargs):
-        context ={}
+        posts = Post.objects.all()
+        context ={
+            'posts': posts
+        }
         return render(request, 'Blog_list.html', context)
 
 class Blog_create_view(View):
@@ -15,6 +19,24 @@ class Blog_create_view(View):
             'form': form
         }
         return render(request, 'blog_create.html', context)
+    
     def post(self, request, *arg, **kwargs):
-        context={}
+        if request.method=="POST":
+            form = Post_Create_Forms(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+                p, created = Post.objects.get_or_create(title=title, content=content)
+                p.save()
+                return redirect('blog:home')
+        context={
+        }
         return render(request, 'blog_create.html', context)
+
+class BlogDetailView(View):
+    def get(self, request, pk, *arg, **kwargs):
+        post = get_object_or_404(Post, pk=pk)
+        context = {
+            'post':post
+        }
+        return render(request,'blog_detail.html', context)
